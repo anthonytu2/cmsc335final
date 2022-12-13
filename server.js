9318a5a462ae6833a81acfafd163cd12583ba35b
Anthony <anthonytu.s.work@gmail.com>
2022-12-12 20:35:30 -0500

    unfinished routing code

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const { response } = require("express");
 process.stdin.setEncoding("utf8");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
+const portNumber = process.argv[2] || 5000;
+console.log(`Visit http://localhost:${portNumber}`);
+
 
 // DeepL Translator API 
 const options = {
@@ -39,3 +42,60 @@ const req = http.request(options, function (res) {
 
 req.write(JSON.stringify({text: 'This is a example text for translation.', source: 'EN', target: 'ES'}));
 req.end();
+
+currentUser = "guest"
+
+// routing section start
+app.set("views", path.resolve(__dirname, "templates"));
+app.set("view engine", "ejs");
+
+app.get("/", (request, response)=>{
+
+	response.render("welcome", {portNumber:portNumber});
+
+});
+
+app.get("/translate", (request, response)=>{
+	let {username, password, original, translation} = request.body;
+	currentUser = username || "guest";
+	original = original || "";
+	translation = translation || "";
+	console.log("tranlate get")
+	response.render("translator", {portNumber:portNumber, username:currentUser, original:original, translation:translation});
+
+});
+
+app.post("/translate", (request, response)=>{
+	let {username, password, original, translation} = request.body;
+	currentUser = username || currentUser;
+	original = original || "";
+	translation = translation || "";
+	console.log("translate post")
+	response.render("translator", {portNumber:portNumber, username:currentUser, original:original, translation:translation});
+
+});
+
+app.get("/signup", (request, response)=>{
+
+	response.render("signup", {portNumber:portNumber});
+
+});
+
+app.get("/signup", (request, response)=>{
+
+	response.render("signup", {portNumber:portNumber});
+
+});
+
+app.get("/log", (request, response)=>{
+
+	response.render("log", {portNumber:portNumber});
+
+});
+
+
+
+app.listen(portNumber);
+
+
+//routing section end
