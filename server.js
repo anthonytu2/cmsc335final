47b70f2d441392a3e06a36977fbb1c08c16f57b4
muchlikehold <syucyonic@gmail.com>
2022-12-14 16:51:20 -0500

    the api translator is working

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch');
 const http = require("http");
 const path = require("path");
 const bodyParser = require("body-parser");
@@ -14,7 +15,6 @@ console.log(`Visit http://localhost:${portNumber}`);
 
 // Microsoft Translator Text API
 // Starter code
-const fetch = require('node-fetch');
 
 const url = 'https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=%3CREQUIRED%3E&api-version=3.0&profanityAction=NoAction&textType=plain';
 
@@ -22,16 +22,11 @@ const options = {
   method: 'POST',
   headers: {
     'content-type': 'application/json',
-    'X-RapidAPI-Key': '252feb1770msh525fa056a0b9266p16d177jsnefa8d131f1e8',
+    'X-RapidAPI-Key': '42e2f88aa4msh9b9e63c519efcd4p1e7f5bjsn71acb1cbaca4',
     'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
   },
   body: '[{"Text":"I would really like to drive your car around the block a few times."}]'
 };
-
-fetch(url, options)
-	.then(res => res.json())
-	.then(json => console.log(json))
-	.catch(err => console.error('error:' + err));
 
 // routing section start
 app.set("views", path.resolve(__dirname, "templates"));
@@ -45,24 +40,44 @@ app.get("/", (request, response)=>{
 
 app.get("/translate", (request, response)=>{
 	let {username, password, original} = request.body;
-	currentUser = username || currentUser;
-	original = "";
-	translation = "";
-	console.log("tranlate get")
-	response.render("translator", {portNumber:portNumber, username:currentUser, original:original, translation:translation});
+	let currentUser = username;
+	original = request.query.lang1Text;
+	let translation = "";
 
+	let lang = "en";
+
+	if (lang == "spanish") {
+		lang = "es";
+	}
+
+	const options = {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json',
+			'X-RapidAPI-Key': '42e2f88aa4msh9b9e63c519efcd4p1e7f5bjsn71acb1cbaca4',
+			'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
+		},
+		body: `[{"Text":"${original}"}]`
+	};
+	
+	fetch(`https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=es&api-version=3.0&profanityAction=NoAction&textType=plain`, options)
+	.then(response => response.json())
+	.then(res => {let resJSON = res[0]
+		translation = resJSON.translations[0].text
+		response.render("translator", {portNumber:portNumber, username:currentUser, original:original, translation:translation});
+	})
+	.catch(err =>{ console.error(err)
+		response.render("translator", {portNumber:portNumber, username:currentUser, original:original, translation:translation});
+	});
+
+
+	console.log(translation)
 });
 
 app.post("/translate", (request, response)=>{
 	let {username, password, original} = request.body;
-	currentUser = username || currentUser;
-	original = original || "";
-	
-	translation = "";
-	/*
-	TODO: use api to convert original text into translation
-	*/
-
+	let currentUser = username || currentUser;
+	let translation = "";
 	console.log("translate post")
 	response.render("translator", {portNumber:portNumber, username:currentUser, original:original, translation:translation});
 
