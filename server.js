ab126826f93f4cc8f323037351256392d50456b3
Rohan Bhattarai <rohan.bhattarai87@gmail.com>
2022-12-16 03:03:07 -0500

    Added Mongo Db functions and finished up some functions that use them Still have some mongo functions left to implement

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch');
 const http = require("http");
 const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, 'credentials/.env') })
 const bodyParser = require("body-parser");
 const fs = require("fs");
 const express = require("express");
@@ -8,15 +9,32 @@ const app = express();
 const { response } = require("express");
 const e = require('express');
 
+// MongoDB imports 
+const userName = process.env.MONGO_DB_USERNAME;
+const passWord = process.env.MONGO_DB_PASSWORD;
+const databaseAndCollection = {db: process.env.MONGO_DB_NAME, collection: process.env.MONGO_COLLECTION};
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 process.stdin.setEncoding("utf8");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 const portNumber = process.argv[2] || 5000;
 console.log(`Visit http://localhost:${portNumber}`);
 
+// Connecting to the Mongo Databse
+const uri = `mongodb+srv://${userName}:${passWord}@cluster0.ae9zey0.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function hi(){try {
+	await client.connect();
+} catch(e) {
+	console.log("uh oh");
+	console.log(e);
+}}
+hi();
+
 // Microsoft Translator Text API
 // Starter code
-
 const url = 'https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=%3CREQUIRED%3E&api-version=3.0&profanityAction=NoAction&textType=plain';
 
 const options = {
@@ -64,8 +82,25 @@ app.get("/translate", (request, response)=>{
 	
 	fetch(`https://microsoft-translator-text.p.rapidapi.com/translate?to%5B0%5D=${lang}&api-version=3.0&profanityAction=NoAction&textType=plain`, options)
 	.then(response => response.json())
-	.then(res => {let resJSON = res[0]
+	.then(async res => {
+		let resJSON = res[0]
 		translation = resJSON.translations[0].text
+		// adding the translation to the history of the user
+		const options2 = {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+				'X-RapidAPI-Key': '42e2f88aa4msh9b9e63c519efcd4p1e7f5bjsn71acb1cbaca4',
+				'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
+			},
+			body: `[{"Text":"${original}"}]`
+		};
+
+		/* 
+		TODO: Detect the language of the original and save it to lang1
+		*/
+		const lang1 = "place holder"
+		await insertTrans(client, databaseAndCollection, username, {lang1: lang1, original: original, lang2: lang, translation: translation});
 		console.log(translation)
 		response.render("translator", {portNumber:portNumber, username:currentUser, original:original, translation:translation});
 	})
@@ -73,61 +108,96 @@ app.get("/translate", (request, response)=>{
 		response.render("translator", {portNumber:portNumber, username:currentUser, original:original, translation:translation});
 	});
 
-
 	console.log(translation)
 });
 
-app.post("/translate", (request, response)=>{
+
+app.post("/translate", async (request, response)=>{
 	let {username, password, original} = request.body;
 	let currentUser = username || currentUser;
 	let translation = "";
+	// TODO: Make sure to clear the guest history
+	if(currentUser === "guest"){
+		await clearGuestHistory(client, databaseAndCollection);
+	}
 	console.log("translate post")
 	response.render("translator", {portNumber:portNumber, username:currentUser, original:original, translation:translation});
-
 });
 
 app.get("/signup", (request, response)=>{
-
 	response.render("signup", {portNumber:portNumber});
-
 });
 
-app.post("/signup", (request, response)=>{
+app.post("/signup", async (request, response)=>{
 	let {username, password} = request.body;
 	console.log("signing up")
 	/*
 	TODO: search database if username already exists, 
 	*/
-	if (Math.random() < .5){
+	// Search database to check if username already exists
+	const result = await lookupUser(client, databaseAndCollection, username);
+	if(result){
 		response.render("signupFail", {username:username})
-	}
-	else {
-		currentUser = username
+	} else {
+		// add the user to the database
+		const user = {
+			username:username,
+			password:password,
+			history:[]
+		}
+		await insertUser(user);
 		response.render("signupConfirm", {username:username});
-
 	}
-
-
 });
 
-/*
-*TODO: functionality to create a table from a users previous translations and send them in for the get
-*/
-function makeTable(){
+// Creates a table from a users previous translations and send them in for the get
+async function makeTable(username){
 	table = ""
-
+	// create the table here
+	const result = await lookupUser(username);
+	// There will always be a user (Guest or an actual one)
+	result.history.forEach(elem => {
+		table += '<tr>';
+		table += `<td>${elem.lang1}</td>`;
+		table += `<td>${elem.original}</td>`;
+		table += `<td>${elem.lang2}</td>`;
+		table += `<td>${elem.translation}</td>`;
+		table += '</tr>';
+	})
 	return table;
 }
 
-app.get("/log", (request, response)=>{
-
-	response.render("log", {portNumber:portNumber, table:"table stuff"});
+app.get("/log", async (request, response)=>{
+	const username = document.getElementById("username").innerText;
+	table = await makeTable(username);
+	response.render("log", {portNumber:portNumber, table:table});
 
 });
-
 
 
 app.listen(portNumber);
 
-
 //routing section end
+
+// MongoDB section start
+
+// Checks to see if the username exits in the database
+async function lookupUser(client, databaseAndCollection, username){
+	const result = await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).find(username);
+	return result;
+}
+
+// Inserts the username and password into the database
+async function insertUser(client, databaseAndCollection, user){
+	await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).insertOne(user);
+}
+
+// Add new translations into the history of the user
+async function insertTrans(client, databaseAndCollection, username, historyTuple){
+	await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).updateOne(username, {$push:historyTuple});
+}
+
+// Clear the guest history 
+async function clearGuestHistory(client, databaseAndCollection){
+	await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).updateOne({username:'guest'}, {$set:{history: []}})
+}
